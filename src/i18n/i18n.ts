b3036581139894aa950211e31ecdbd01c6b77947
Yuriy <>
2025-06-06 21:59:44 +0300

    Update i18n.ts

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -25,31 +25,31 @@
 
 // export default i18n;
 
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import { store } from "../redux/store";
-import { en } from "../lib/locales/en";
-import { ua } from "../lib/locales/ua";
-import { de } from "../lib/locales/de";
+// import i18n from "i18next";
+// import { initReactI18next } from "react-i18next";
+// import { store } from "../redux/store";
+// import { en } from "../lib/locales/en";
+// import { ua } from "../lib/locales/ua";
+// import { de } from "../lib/locales/de";
 
-if (typeof window !== "undefined") {
-  import("i18next-browser-languagedetector").then(
-    ({ default: LanguageDetector }) => {
-      i18n
-        .use(LanguageDetector)
-        .use(initReactI18next)
-        .init({
-          resources: {
-            en: en,
-            de: de,
-            ua: ua,
-          },
-          lng: store.getState().language.currentLanguage,
-          fallbackLng: "en",
-          interpolation: { escapeValue: false },
-        });
-    }
-  );
-}
+// if (typeof window !== "undefined") {
+//   import("i18next-browser-languagedetector").then(
+//     ({ default: LanguageDetector }) => {
+//       i18n
+//         .use(LanguageDetector)
+//         .use(initReactI18next)
+//         .init({
+//           resources: {
+//             en: en,
+//             de: de,
+//             ua: ua,
+//           },
+//           lng: store.getState().language.currentLanguage,
+//           fallbackLng: "en",
+//           interpolation: { escapeValue: false },
+//         });
+//     }
+//   );
+// }
 
-export default i18n;
+// export default i18n;
