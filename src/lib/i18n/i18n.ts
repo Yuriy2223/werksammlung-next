44ce8924d5eb970fa80c4843e3f4f18920e40dfa
Yuriy <>
2025-06-11 19:55:10 +0300

    Update i18n.ts

--- src/lib/i18n/i18n.ts
+++ src/lib/i18n/i18n.ts
@@ -1,6 +1,5 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 import { en } from "./locales/en";
 import { ua } from "./locales/ua";
 import { de } from "./locales/de";
@@ -8,21 +7,43 @@ import { de } from "./locales/de";
 const resources = { en, ua, de };
 
 if (!i18n.isInitialized) {
-  i18n
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-      resources,
-      fallbackLng: "en",
-      interpolation: {
-        escapeValue: false,
-      },
-      detection: {
-        order: ["localStorage", "navigator"],
-        caches: [],
-        // caches: ["localStorage"],
-      },
-    });
+  i18n.use(initReactI18next).init({
+    resources,
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 }
 
 export default i18n;
+
+// import i18n from "i18next";
+// import { initReactI18next } from "react-i18next";
+// import LanguageDetector from "i18next-browser-languagedetector";
+// import { en } from "./locales/en";
+// import { ua } from "./locales/ua";
+// import { de } from "./locales/de";
+
+// const resources = { en, ua, de };
+
+// if (!i18n.isInitialized) {
+//   i18n
+//     .use(LanguageDetector)
+//     .use(initReactI18next)
+//     .init({
+//       resources,
+//       fallbackLng: "en",
+//       interpolation: {
+//         escapeValue: false,
+//       },
+//       detection: {
+//         order: ["localStorage", "navigator"],
+//         caches: [],
+//         // caches: ["localStorage"],
+//       },
+//     });
+// }
+
+// export default i18n;
