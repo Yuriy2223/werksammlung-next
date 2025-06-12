db206ff30c96d3d4a303d2f907c066e1d147a4cb
Yuriy <>
2025-06-12 16:53:33 +0300

    update package.json

--- src/lib/i18n/i18n.ts
+++ src/lib/i18n/i18n.ts
@@ -46,32 +46,3 @@ export const initI18n = (language: string = "en") => {
 };
 
 export default i18n;
-
-// import i18n from "i18next";
-// import { initReactI18next } from "react-i18next";
-// import LanguageDetector from "i18next-browser-languagedetector";
-// import { en } from "./locales/en";
-// import { ua } from "./locales/ua";
-// import { de } from "./locales/de";
-
-// const resources = { en, ua, de };
-
-// if (!i18n.isInitialized) {
-//   i18n
-//     .use(LanguageDetector)
-//     .use(initReactI18next)
-//     .init({
-//       resources,
-//       fallbackLng: "en",
-//       interpolation: {
-//         escapeValue: false,
-//       },
-//       detection: {
-//         order: ["localStorage", "navigator"],
-//         caches: [],
-//         // caches: ["localStorage"],
-//       },
-//     });
-// }
-
-// export default i18n;
