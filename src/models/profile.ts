1b9ab52c2a7d2ba9a753968609b75880f1c0d463
Yuriy <>
2025-07-05 23:07:45 +0300

    Update profile.ts

--- src/models/profile.ts
+++ src/models/profile.ts
@@ -65,8 +65,6 @@ profileSchema.methods.toJSON = function () {
       obj.avatarUrl.contentType
     };base64,${obj.avatarUrl.data.toString("base64")}`;
   }
-  //  Повертаємо тільки URL до аватарки
-  // obj.avatarUrl = obj.avatarUrl?.data ? `/api/uploads/avatar/${obj._id}` : null;
 
   if (obj.viewCV?.data) {
     obj.viewCV = `/api/uploads/cv/${obj._id}`;
